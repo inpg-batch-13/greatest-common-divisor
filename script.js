fdcb5b8de8dcc3cb65a8e0fc1ecac2ef93030eef
rahmatrhd <kickyou44@gmail.com>
2019-06-19 21:00:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function gcd (firstNumber, secondNumber) {
+  // Your code here
+}
+
+// TEST CASES
+console.log(gcd(12, 16)); // 4
+console.log(gcd(50, 40)); // 10
+console.log(gcd(22, 99)); // 11
+console.log(gcd(24, 36)); // 12
+console.log(gcd(17, 23)); // 1
