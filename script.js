3ba04c75d86e4fe7d9358e3130b0bdd50a82cfb1
MuhammadFahmiRidwan <mfahmiridwan50@gmail.com>
2019-06-19 21:46:59 +0700

    done

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function gcd (firstNumber, secondNumber) {
   // Your code here
+  var hasil = 0
+  for (var i = 1; i < firstNumber; i++) {
+     if(firstNumber % i === 0 && secondNumber % i === 0){
+    hasil = i
+  }
+}
+ return hasil
 }
 
 // TEST CASES
@@ -8,3 +15,4 @@ console.log(gcd(50, 40)); // 10
 console.log(gcd(22, 99)); // 11
 console.log(gcd(24, 36)); // 12
 console.log(gcd(17, 23)); // 1
+console.log(gcd(50, 1)) // 0
